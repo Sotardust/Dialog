8987539e67866e29ac726c2f54baf6b2c00ca8b3
daihaitao <daihaitao@wanwujieshu.net>
2017-09-07 15:04:06 +0800

    init

---
+++ app/src/main/java/com/dai/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dai;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dai on 2017/9/7.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
